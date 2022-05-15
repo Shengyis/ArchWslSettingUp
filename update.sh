50b174d16ba65371d9324a8e42a34ee5971f5c3e
Shengyis <g.shengyis@gmail.com>
2022-05-15 10:46:21 +0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/zsh
 
+sleep 2
+echo ur_passwd | sudo -v -S
+echo "\n"
 echo "\033[31m[System upgrade]\033[0m"
-echo ur_passwd | sudo -v
-sudo pacman -Syyu --noconfirm
+yay -Syyu --noconfirm
 
 echo "\033[31m[Distrod update]\033[0m"
 sudo ${HOME}/.distrod_install.sh update
 
 echo "\033[31m[Clear up]\033[0m"
-sudo pacman -Scc --noconfirm
+yay -Scc --noconfirm
 
 orphaned_list=$(pacman -Qdqt)
 if [ ! ${orphaned_list} ]
 then
 	echo "No orphaned package"
 else
-	sudo pacman -Rsn ${orphaned_list} --noconfirm
+	sudo yay -Rsn ${orphaned_list} --noconfirm
 fi
