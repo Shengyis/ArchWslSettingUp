492ff6741c44d3b8a1921802dd382e37937d092d
Shengyis <g.shengyis@gmail.com>
2022-05-17 22:01:15 +0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,21 +1,36 @@
 #!/usr/bin/zsh
 
 sleep 2
-echo ur_passwd | sudo -v -S
-echo "\n"
-echo "\033[31m[System upgrade]\033[0m"
-yay -Syyu --noconfirm
+echo -n "Check updates? [Y/N] "
+read input
 
-echo "\033[31m[Distrod update]\033[0m"
-sudo ${HOME}/.distrod_install.sh update
+case ${input} in
+	[yY][eE][sS]|[yY])
 
-echo "\033[31m[Clear up]\033[0m"
-yay -Scc --noconfirm
+		echo chinasky | sudo -v -S
+		echo "\n"
+		echo "\033[31m[System upgrade]\033[0m"
+		yay -Syyu --noconfirm
 
-orphaned_list=$(pacman -Qdqt)
-if [ ! ${orphaned_list} ]
-then
-	echo "No orphaned package"
-else
-	sudo yay -Rsn ${orphaned_list} --noconfirm
-fi
+		echo "\033[31m[Distrod update]\033[0m"
+		sudo ${HOME}/.distrod_install.sh update
+
+		echo "\033[31m[Clear up]\033[0m"
+		yay -Scc --noconfirm
+
+		orphaned_list=$(pacman -Qdqt)
+		if [ ! ${orphaned_list} ]
+		then
+			echo "No orphaned package"
+		else
+			yay -Rsn ${orphaned_list} --noconfirm
+		fi
+		;;
+
+	[nN][oO]|[nN])
+		;;
+	
+	*)
+		echo "Invalid input"
+		;;
+esac
