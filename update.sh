db986dfd713a3ef196435b7919c9aecd3806cd15
Shengyis <g.shengyis@gmail.com>
2022-05-22 12:37:23 +0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,36 +1,20 @@
 #!/usr/bin/zsh
 
-sleep 2
-echo -n "Check updates? [Y/N] "
-read input
+sleep 1
+echo chinasky | sudo -v -S
+echo "\n"
+echo "\033[31m[System upgrade]\033[0m"
+yay -Syyu --noconfirm
 
-case ${input} in
-	[yY][eE][sS]|[yY])
+echo "\033[31m[Distrod update]\033[0m"
+sudo ${HOME}/.distrod_install.sh update
 
-		echo chinasky | sudo -v -S
-		echo "\n"
-		echo "\033[31m[System upgrade]\033[0m"
-		yay -Syyu --noconfirm
+echo "\033[31m[Clear up]\033[0m"
+yay -Scc --noconfirm
 
-		echo "\033[31m[Distrod update]\033[0m"
-		sudo ${HOME}/.distrod_install.sh update
-
-		echo "\033[31m[Clear up]\033[0m"
-		yay -Scc --noconfirm
-
-		orphaned_list=$(pacman -Qdqt)
-		if [ ! ${orphaned_list} ]
-		then
-			echo "No orphaned package"
-		else
-			yay -Rsn ${orphaned_list} --noconfirm
-		fi
-		;;
-
-	[nN][oO]|[nN])
-		;;
-	
-	*)
-		echo "Invalid input"
-		;;
-esac
+orphaned_list=$(pacman -Qdqt)
+if [[ !${orphaned_list} ]] {
+	echo "No orphaned package"
+} else {
+	yay -Rsn ${orphaned_list} --noconfirm
+}
