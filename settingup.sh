377b2d7ab15fa5fc349feab3b38af8f823630c05
Shengyis <g.shengyis@gmail.com>
2022-09-28 20:36:58 +0800

    Update settingup.sh

--- settingup.sh
+++ settingup.sh
@@ -2,9 +2,8 @@
 
 # Enable systemd in wsl
 curl -L -O "https://raw.githubusercontent.com/nullpo-head/wsl-distrod/main/install.sh"
-chmod +x install.sh
-./install.sh install
-./install.sh update
-/opt/distrod/bin/distrod enable
+touch /etc/wsl.conf
+echo "[boot]" >> /etc/wsl.conf
+echo "systemd=true" >> /etc/wsl.conf
 
 echo "restart and run settingup2.sh"
