5358fe24d18819f0c73bd399e67f58be70cdb66f
Shengyis <g.shengyis@gmail.com>
2022-10-31 11:48:57 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,14 +5,14 @@ echo password | sudo -v -S
 echo "\n"
 
 echo "\033[31m[System upgrade]\033[0m"
-pacman -Syyu --noconfirm
+sudo pacman -Syyu --noconfirm
 
 echo "\033[31m[Clear up]\033[0m"
-pacman -Scc --noconfirm
+sudo pacman -Scc --noconfirm
 
 orphaned_list=$(pacman -Qdqt)
 if [[ !${orphaned_list} ]] {
 	echo "No orphaned package"
 } else {
-	pacman -Rsn ${orphaned_list} --noconfirm
+	sudo pacman -Rsn ${orphaned_list} --noconfirm
 }
