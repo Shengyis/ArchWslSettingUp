b8e71ba8b66615d82e511016c1c24231e4239ace
Shengyis <g.shengyis@gmail.com>
2022-10-31 11:24:51 +0800

    change to pacman instead of yay

--- update.sh
+++ update.sh
@@ -3,14 +3,12 @@
 sleep 1
 echo password | sudo -v -S
 echo "\n"
-echo "\033[31m[Distrod update]\033[0m"
-sudo ${HOME}/.distrod_install.sh update
 
 echo "\033[31m[System upgrade]\033[0m"
-yay -Syyu --noconfirm
+pacman -Syyu --noconfirm
 
 echo "\033[31m[Clear up]\033[0m"
-yay -Scc --noconfirm
+pacman -Scc --noconfirm
 
 orphaned_list=$(pacman -Qdqt)
 if [[ !${orphaned_list} ]] {
