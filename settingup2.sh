52a295c11939c4fb4da988816f3dac8602ab9156
Shengyis <ms.shengyis@outlook.com>
2023-05-28 08:57:22 +0800

    Update settingup2.sh

--- settingup2.sh
+++ settingup2.sh
@@ -70,7 +70,7 @@ rm .bash*
 #install user packages 
 su - ${username} << EOF
 echo ${password} | sudo -v -S
-sudo pacman -S openblas eigen python tk python-matplotlib python-scipy python-mpmath --noconfirm
+sudo pacman -S openblas python tk python-matplotlib python-scipy python-mpmath ghostscript popplar --noconfirm
 EOF
 
 echo "restart and run settingup3.sh"
