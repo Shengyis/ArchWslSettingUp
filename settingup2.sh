b7b75f69257892d82a9f37e7fae3f028dc7fb600
Shengyis <ms.shengyis@outlook.com>
2023-11-13 09:42:29 +0800

    Update settingup2.sh

--- settingup2.sh
+++ settingup2.sh
@@ -33,7 +33,7 @@ sed -i '/NoProgressBar/s/^/#/' /etc/pacman.conf
 # Update arch and install gvim, base-devel, git, wget, cronie, system fonts, zsh
 pacman-key --init
 pacman-key --populate archlinux
-pacman -Syyu gvim git wget adobe-source-sans-fonts cronie base-devel zsh --noconfirm
+pacman -Syyu gvim git wget adobe-source-code-pro-fonts cronie base-devel zsh --noconfirm
 
 # let wheel group use sudo
 sed -i '0, /%wheel/s/^# //' /etc/sudoers
