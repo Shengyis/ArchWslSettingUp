8f27f9a202adaa25eabc2e6167f26276e011dbbd
Shengyis <ms.shengyis@outlook.com>
2023-12-04 10:58:06 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ echo "\033[31m[Clear up]\033[0m"
 sudo pacman -Scc --noconfirm
 
 orphaned_list=$(pacman -Qdqt)
-if [[ -v ${orphaned_list} ]] {
+if [[ -n "${orphaned_list}" ]] {
 	sudo pacman -Rsn ${orphaned_list} --noconfirm
 } else {
 	echo "No orphaned package"
